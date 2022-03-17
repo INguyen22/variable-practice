117013f79d2e5f44e97df65e07f48ce25e47c10d
Ivy Nguyen <animeweirdo24@gmail.com>
2022-03-17 14:23:28 -0600

    Annotate file

--- variables.js
+++ variables.js
@@ -1,9 +1,18 @@
+//variable email declared and assigned to the string of "ivyn2464@gmail.com"
 var email = "ivyn2464@gmail.com"
+//variable name declared and assigned to the string of "ivy"
 var name = "ivy"
+//variable gender declared and assigned to the string of "female"
 var gender = "female"
+//variable age declared and assigned to the number of 22
 var age = 22
+//variable familySize declared and assigned to the number of 6
 var familySize = 6
+//variable friends declared and assigned to the number of 3
 var friends = 3
+//variable hungry decleared and assigned to the boolean of true
 var hungry = true
+//variable hasSanity declared and is assigned to the boolean of false
 var hasSanity = false
+//variable havingFun declared and is assigned to the boolean of true
 var havingFun = true
